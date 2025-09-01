0f003a847114dd63ce096f5c2f5e46cfbec1aa9e
Ry0nan <125460295+Ry0nan@users.noreply.github.com>
2025-09-01 22:46:29 +0800

    added a Gameboard IIFE function

---
+++ script.js
@@ -0,0 +1,40 @@
+const Gameboard = (function() {
+
+    // 1D private array
+    let board = ["", "", "", "", "", "", "", "", ""];
+
+    // return the board array
+    function getBoard() {
+        return [...board];
+    };
+
+    // puts "X" or "O" at a given spot if it is empty
+    // checks first if there is a 'mark' already in the index
+    // if none then put the mark in the index and return true
+    // if there is, return false
+    function setMark(index, mark) {
+        if (board[index] === "") {
+            board[index] = mark;
+            return true;
+        }
+        return false;
+    };
+
+    // empty out the board
+    function resetBoard() {
+        board = ["", "", "", "", "", "", "", "", ""];
+    };
+
+
+    return {getBoard, setMark, resetBoard};
+
+})();
+
+
+// test codes
+// testing to see if the board returns properly
+console.log(Gameboard.getBoard());
+
+// testing to see if the board properly checks if the spot is filled 
+Gameboard.setMark(0, "X"); 
+console.log(Gameboard.getBoard()); // ["X", "", "", "", "", "", "", "", ""]
